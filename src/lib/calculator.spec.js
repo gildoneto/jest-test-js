22d3daf9d4cd353b13db70a391ce420d559f566c
Gildo Neto <gildo.neto@telus.com>
2022-09-26 15:44:32 -0300

    finaliza calculator

--- src/lib/calculator.spec.js
+++ src/lib/calculator.spec.js
@@ -7,3 +7,18 @@ it('should sum 2 and 2 and the result must be 4', () => {
 it('should sum 2 and 2 even if one of them is a string', () => {
   expect(sum('2','2')).toBe(4);
 });
+
+it('should thrown an error if what is provided to the method cannot be summed', () => {
+  expect(() => {
+    sum('', 2)
+  }).toThrowError();
+  expect(() => {
+    sum([2, 2])
+  }).toThrowError();
+  expect(() => {
+    sum({})
+  }).toThrowError();
+  expect(() => {
+    sum()
+  }).toThrowError();
+});
