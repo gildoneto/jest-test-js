f4a2ca742a426259aff72e2a5b3eed52dbf5b9c2
Gildo Neto <gildo.neto@telus.com>
2022-09-28 11:08:48 -0300

    testa o debugger ndb chromium

--- src/lib/discount.utils.js
+++ src/lib/discount.utils.js
@@ -13,6 +13,7 @@ const calculatePercentageDiscount = (amount, { condition, quantity }) => {
 };
 
 const calculateQuantityDiscount = (amount, { condition, quantity }) => {
+  debugger; // para ser testado com o ndb
   const isEven = quantity % 2 === 0;
   if (condition?.quantity && quantity > condition.quantity) {
     return amount.percentage(isEven ? 50 : 40);
